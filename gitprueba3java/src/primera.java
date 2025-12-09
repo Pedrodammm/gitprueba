c516c082ec583bf7b719da15178c49b48c45f3d9
Pedro Delgado <pdelmen793@g.educaand.es>
2025-12-09 17:04:34 +0100

    commit merge4

--- gitprueba3java/src/primera.java
+++ gitprueba3java/src/primera.java
@@ -3,6 +3,21 @@ public class primera {
 
         int a = 0;
         int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        int f = 0;
+        int g = 0;
+        int h = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        int n = 0;
+        int o = 0;
+        int p = 0;
+        int q = 0;
 
     }
 }
